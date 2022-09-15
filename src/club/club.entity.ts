608bdbe2d5a643a184b9b306f3e76f840fdb8bfe
Cristian <cl.arango741@uniandes.edu.co>
2022-09-14 22:56:06 -0500

    persistencia completada

---
+++ src/club/club.entity.ts
@@ -0,0 +1,18 @@
+import { SocioEntity } from 'src/socio/socio.entity';
+import { Entity, Column, PrimaryGeneratedColumn,ManyToMany} from 'typeorm';
+
+@Entity()
+export class ClubEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id:string;
+  @Column()
+  nombre:string;
+  @Column()
+  fechaFundacion: Date;
+  @Column()
+  imagenUrl: string;
+  @Column()
+  descripcion:string;
+  @ManyToMany(()=> SocioEntity, socio => socio.clubs)
+  socios: SocioEntity[]
+}
---
+++ src/socio/socio.entity.ts
@@ -0,0 +1,16 @@
+import { ClubEntity } from 'src/club/club.entity';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToMany} from 'typeorm';
+
+@Entity()
+export class SocioEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column()
+  usuario: string;
+  @Column()
+  correo: string;
+  @Column()
+  fechaNacimiento: Date;
+  @ManyToMany(()=> ClubEntity, club => club.socios)
+  clubs: ClubEntity[]
+}
